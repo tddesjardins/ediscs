6a69b3a65e19cd5cb7d555dba380b41baf2c6559
Tyler Desjardins <tddesjar@gmail.com>
2016-12-06 07:36:48 -0600

    Updated FITS column names

--- eazy.py
+++ eazy.py
@@ -5,16 +5,21 @@ import matplotlib.pyplot as plt
 import threedhst.eazyPy as eazy
 import numpy as np
 from astropy.stats.funcs import biweight_location as bl
+from astropy.stats.funcs import biweight_midvariance as bs
 
 flux={'cB':'fB1','kB':'fB1','V':'fV1','R':'fR1','I':'fI1','z':'fz1','K':'fK1'}
 corrflux={'cB':'cfB1','kB':'cfB1','V':'cfV1','R':'cfR1','I':'cfI1','z':'cfz1','K':'cfK1'}
 err={'cB':'fB1err','kB':'fB1err','V':'fV1err','R':'fR1err','I':'fI1err','z':'fz1err','K':'fK1err'}
 fabbrv={'cB':'F1','kB':'F2','V':'F3','R':'F4','I':'F5','z':'F6','K':'F7'}
 eabbrv={'cB':'E1','kB':'E2','V':'E3','R':'E4','I':'E5','z':'E6','K':'E7'}
-klam={'cB':4.125,'V':3.060,'R':2.548,'I':1.993,'z':1.384,'K':0.367}
+klam={'kB':4.125,'cB':4.125,'V':3.060,'R':2.548,'I':1.993,'z':1.384,'K':0.367}
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='palatino')
+
+def sz(phot,spec):
+
+    return np.mean(np.abs((phot-spec))/(1.+spec))
 
 def dered(filter, flux, ebv):
 
@@ -23,53 +28,90 @@ def dered(filter, flux, ebv):
     
     return drflux
 
-def plotphotspec(catalog,zphot='OUTPUT/photz.zout',output='zspec_zphot.pdf',selection='',savetxt=False):
+def plotphotspec(catalog,zphot='OUTPUT/photz.zout',output='zspec_zphot.pdf',selection='',savetxt=False,autorange=False,maxErr=0.1):
 
-    if selection == '':
-        selection='zLDP < 2.0'
+    if type(maxErr) is not str:
+        smaxErr=str(maxErr)
     else:
-        selection=selection+' & zLDP < 2.0'
+        smaxErr=maxErr
     
     obsData=Table.read(catalog).to_pandas()
     index=range(len(obsData))
     obsData['index']=index
     obsData['Rmag']=-2.5*np.log10(obsData['fRauto'])+23.9
+
+    if selection == '':
+        selection='zLDP5_7 < 2.0 & Q5_7 == 4'
+    else:
+        selection=selection+' & zLDP5_7 < 2.0 & Q5_7 == 4'
+    
     trimData=obsData.query(selection)
     
     z=eazy.catIO.Readfile(zphot)
-    trimData['zphot']=z.z_p[trimData['index']].tolist()
+    trimData['zphot']=z.z_m2[trimData['index']].tolist()
+    trimData['u68']=z.u68[trimData['index']].tolist()-trimData['zphot']
+    trimData['l68']=trimData['zphot']-z.l68[trimData['index']].tolist()
+    trimData['residual']=trimData['zphot']-trimData['zLDP5_7']
 
     #Zphot vs. ZLDP plot
+    filled=trimData.query('((residual < 0.) and (u68 < '+smaxErr+')) or ((residual > 0.) and (l68 < '+smaxErr+'))')
+    unfilled=trimData.query('((residual < 0.) and (u68 > '+smaxErr+')) or ((residual > 0.) and (l68 > '+smaxErr+'))')
     plt.plot([-10,10],[-10,10],'k-')
-    plt.plot(trimData['zLDP'],trimData['zphot'],'ro')
+    plt.plot(filled['zLDP5_7'],filled['zphot'],'ro')
+    plt.plot(unfilled['zLDP5_7'],unfilled['zphot'],'ro',markerfacecolor='none')
 
-    zmax=np.max(trimData['zLDP'])
-    plt.axis([0,zmax,0,zmax])
+    zmax=np.max(trimData['zLDP5_7'])
+    if autorange == True:
+        plt.axis([0,zmax,0,zmax])
+    else:
+        plt.axis([0.,1.,0.,1.])
     plt.xlabel(r'$z_{LDP}$')
-    plt.ylabel(r'$z_{peak}$')
+    plt.ylabel(r'$z_{z\_m2}$')
 
     plt.savefig(output, format='pdf', dpi=6000)
     plt.clf()
 
     plt.plot([-10,10],[0,0],'k-')
-    residuals=trimData['zphot']-trimData['zLDP']
-    plt.plot(trimData['zLDP'],residuals,'ro')
+    residuals=trimData['zphot']-trimData['zLDP5_7']
+    filled['residual']=filled['zphot']-filled['zLDP5_7']
+    filledresidual=filled['zphot']-filled['zLDP5_7']
+    plt.plot(trimData['zLDP5_7'],residuals,'ro')
     plt.xlabel(r'$z_{LDP}$')
-    plt.ylabel(r'$z_{peak}-z_{LDP}$')
+    plt.ylabel(r'$z_{z\_m2}-z_{LDP}$')
     plt.plot([0.3,0.3],[-10,10],'k--')
     plt.plot([0.9,0.9],[-10,10],'k--')
-    mean=bl(residuals.values[np.where((trimData['zLDP'].values >= 0.3) & (trimData['zLDP'].values <= 0.9))])
-    plt.axis([0,zmax,-1.5,1.5])
-    plt.text(0.4,0.75,'mean = '+str(np.around(mean,3)))
+    good=np.where((trimData['zLDP5_7'].values >= 0.3) & (trimData['zLDP5_7'].values <= 0.9) & (np.abs(residuals) <= 1.0))
+    mean=bl(residuals.values[good])
+    sig=bs(residuals.values[good])
+    scatter=sz(trimData['zphot'].values[good],trimData['zLDP5_7'].values[good])
+
+    if autorange == True:
+        plt.axis([0,zmax,-1.5,1.5])
+    else:
+        plt.axis([0.,1.,-0.5,0.5])
+    if mean < 0.:
+        sign=r'$-$'
+    else:
+        sign=''
+    plt.text(0.4,0.40,'mean = '+sign+str(np.around(np.abs(mean),3)))
+    plt.text(0.4,0.35,'midvar = '+str(np.around(sig,3)))
+    plt.text(0.4,0.30,r'$\Delta z/(1+z)$ = '+str(np.around(scatter,3)))
 
     plt.savefig('photoz_residuals.pdf', format='pdf', dpi=6000)
     plt.clf()
-
-    trimData['residual']=trimData['zphot']-trimData['zLDP']
-
-    trimData.to_csv('eazy_output.cat',index_label=False,sep='\t')
     
-    pdb.set_trace()
+    trimData['residual']=trimData['zphot']-trimData['zLDP5_7']
+
+    trimData.to_csv('eazy_output.cat',index_label='#index',sep='\t',columns=['field','ids','ra','dec','x','y','zLDP5_7','Q5_7','zphot'])
+
+    good=np.where((filled['zLDP5_7'].values >= 0.3) & (filled['zLDP5_7'].values <= 0.9) & (np.abs(filledresidual) <= 1.0))
+    lineMetric=np.sqrt(np.sum((filled['residual'].values[good])**2.))/len(good)
+    
+    return scatter,lineMetric,trimData
+
+def plotZPoffs():
+
+    tempfilt, coeffs, temp_seds, pz = eazy.readEazyBinary(MAIN_OUTPUT_FILE=root, OUTPUT_DIRECTORY='OUTPUT',CACHE_FILE = 'Same')
 
 def flux2mag(flux):
 
@@ -80,7 +122,7 @@ def plotResult(idnum,catalog,save=False):
     id=str(idnum)
 
     obsData=Table.read(catalog).to_pandas()
-    rmag=-2.5*np.log10(obsData['fR3'][idnum])+23.9
+    rmag=-2.5*np.log10(obsData['fRauto'][idnum])+23.9
     
     axes = eazy.plotExampleSED(idx=idnum, writePNG=False, MAIN_OUTPUT_FILE='photz',
                             OUTPUT_DIRECTORY='OUTPUT', CACHE_FILE='Same', lrange=[3800, 3.e4],
@@ -137,7 +179,7 @@ def prep(catalog,output,filters,defFile='zphot.translate',synthR=False,maglim=99
         obsData.loc[obsData['rmag'] > maglim,x] = -99
         obsData.loc[flux2mag(obsData[x]) < uppercut,x] = -99
         if starcut == True:
-            obsData.loc[obsData['starR'] > 0.1,x] = -99
+            obsData.loc[obsData['class_StarR'] > 0.1,x] = -99
 
     for x in filters:
         obsData[corrflux[x]] = dered(x, obsData[flux[x]], obsData['ebv'])
@@ -153,7 +195,15 @@ def prep(catalog,output,filters,defFile='zphot.translate',synthR=False,maglim=99
         out.write(corrflux[x]+' '+fabbrv[x]+'\n'+err[x]+' '+eabbrv[x]+'\n')
     out.write('cfRauto TOT4\n')
     out.close()
-        
+
+def fluxResPlot(input='OUTPUT/photz.out',outplot='fluxres.pdf'):
+
+    sed = eazy.getEazySED(idnum, MAIN_OUTPUT_FILE='photz', OUTPUT_DIRECTORY='./OUTPUT',
+                        CACHE_FILE='Same', scale_flambda=True, verbose=False,
+                        individual_templates=False)
+
+    lambdaz, temp_sed, lci, obs_sed, fobs, efobs = sed
+
 def main(redfile='OUTPUT/photz.zout',catalog=''):
 
     df=pd.read_table(redfile,delim_whitespace=True,
