eb35703df7b421a46a653c0dcfc8c1b0d5b79b01
Tyler Desjardins <desjard@stsci.edu>
2018-06-10 06:09:02 -0400

    Updating main

--- ediscs_catalog.py
+++ ediscs_catalog.py
@@ -10,6 +10,7 @@ from scipy.optimize import curve_fit
 
 from Util import constants as const
 from Util import ediscs_tools as etools
+from astropy.io import fits
 
 import pdb, shlex, os, shutil, pandas, statsmodels.api as sm, numpy as np
 import matplotlib.pyplot as plt, matplotlib.cm as cm, photcheck as pc, re
@@ -191,55 +192,6 @@ def addquad(xerr,orerr,nrerr,xflux,orflux,nrflux):
     value=(xflux/nrflux)*orflux
 
     return np.abs(value*np.sqrt((xerr/xflux)**2. + (nrerr/nrflux)**2. + (orerr/orflux)**2.))
-    
-    
-#-----------------------------------
-def photscript(listfile,clname,photfile='photscript'):
-    """
-    PURPOSE: Generate a script (to be called on with source from the command line) that will run SExtractor on
-    \tall input images in dual-image mode (using the R-band for detection) for a cluster.
-
-    INPUTS:
-    \tlistfile - two columns of ?img (where ? is the filter letter) and image file names
-    \tclname   - name of the cluster (e.g., cl1354)
-    \tphotfile - name of the output script file (optional)
-
-    RETURNS: None.
-    """
-
-    (keys,files)=np.loadtxt(listfile,usecols=(0,1),unpack=True,dtype={'names':('keys','files'), 'formats':('S5','S30')})
-    imgs={}
-    for x in range(len(keys)):
-        imgs[keys[x]]=files[x]
-        
-    outfile=open('photscript','w')
-    
-    string1='sex -c ediscs.sex -BACKPHOTO_TYPE "GLOBAL" -CATALOG_NAME '
-    string2=' -CHECKIMAGE_TYPE "-BACKGROUND" -CHECKIMAGE_NAME '
-    finalString=''
-
-    finalString=finalString+string1+clname+'_r.cat'+' -CHECKIMAGE_TYPE "-BACKGROUND,SEGMENTATION" -CHECKIMAGE_NAME "' \
-        +imgs['rimg'][:-5]+'_bkgsub.fits'+','+imgs['rimg'][:-5]+'_segmap.fits" '+imgs['rimg']+','+imgs['rimg']+'\n'
-        
-    if 'bimg' in keys:
-        finalString=finalString+string1+clname+'_b.cat'+string2+imgs['bimg'][:-5]+'_bkgsub.fits '+imgs['rimg']+','+imgs['bimg']+'\n'
-    if 'vimg' in keys:
-        finalString=finalString+string1+clname+'_v.cat'+string2+imgs['vimg'][:-5]+'_bkgsub.fits '+imgs['rimg']+','+imgs['vimg']+'\n'
-    if 'iimg' in keys:
-        finalString=finalString+string1+clname+'_i.cat'+string2+imgs['iimg'][:-5]+'_bkgsub.fits '+imgs['rimg']+','+imgs['iimg']+'\n'
-    if 'zimg' in keys:
-        finalString=finalString+string1+clname+'_z.cat'+string2+imgs['zimg'][:-5]+'_bkgsub.fits '+imgs['rimg']+','+imgs['zimg']+'\n'
-    if 'kimg' in keys:
-        finalString=finalString+string1+clname+'_k.cat'+string2+imgs['kimg'][:-5]+'_bkgsub.fits '+imgs['rimg']+','+imgs['kimg']+'\n'
-    if 'rbimg' in keys:
-        finalString=finalString+string1+clname+'_rb.cat'+string2+imgs['rbimg'][:-5]+'_bkgsub.fits '+imgs['rimg']+','+imgs['rbimg']+'\n'
-    if 'rkimg' in keys:
-        finalString=finalString+string1+clname+'_rk.cat'+string2+imgs['rkimg'][:-5]+'_bkgsub.fits '+imgs['rimg']+','+imgs['rkimg']+'\n'
-
-    out=open(photfile,'w')
-    out.write(finalString)
-    out.close()
-        
 
 #-----------------------------------
 def seeingCorr(rr, rm, mm, outfile='seeingcorr.dat'):
@@ -867,47 +819,44 @@ def updateKCat(k='',r='',rk='',zpk='',imglist='imglist',rsegmap='',oldcat='',nul
     tab.write(outname, format='fits', overwrite=True)
 
 #-----------------------------------
-def main(b="", v="", r="", i="", z="", k="", rb="", rk="", imglist='', rsegmap="",
-         zpb=0.0, zpk=0.0, zpz=0.0, null=-99, kpno = False, clname="",pixscale=0.238,
-         outprefix='',idname='',synth=False, megacat=megaLocation,maxz=100.,
+def main(imgs, cluster_name, detection_image='rimg', use_megacat=True,
+         zpb=0.0, zpk=0.0, zpz=0.0, null=-99, kpno = False, ,pixscale=0.238,
+         outprefix='',synth=False, megacat=megaLocation,maxz=100.,
          xmin=-99,xmax=-99,ymin=-99,ymax=-99,errborder=50.0,errs=True,wkimg='',fors='',classStar=0.9):
     """
+    PURPOSE
+    -------
+
+    INPUTS
+    ------
+
+    OUTPUTS
+    -------
     """
 
-    (keys,files)=np.loadtxt(imglist,usecols=(0,1),unpack=True,dtype={'names':('keys','files'), 'formats':('S5','S30')})
-    imgs={}
-    for x in range(len(keys)):
-        imgs[keys[x]]=files[x]
+    # ----------------
+    # Initial set up |
+    # ----------------
 
-    if 'bimg' in imgs:
-        bbkgsub=imgs['bimg'][:-5]+'_bkgsub.fits'
-    if 'vimg' in imgs:
-        vbkgsub=imgs['vimg'][:-5]+'_bkgsub.fits'
-    if 'rimg' in imgs:
-        rbkgsub=imgs['rimg'][:-5]+'_bkgsub.fits'
-    if 'iimg' in imgs:
-        ibkgsub=imgs['iimg'][:-5]+'_bkgsub.fits'
-    if 'zimg' in imgs:
-        zbkgsub=imgs['zimg'][:-5]+'_bkgsub.fits'
-    if 'kimg' in imgs:
-        kbkgsub=imgs['kimg'][:-5]+'_bkgsub.fits'
-    if 'rbimg' in imgs:
-        rbbkgsub=imgs['rbimg'][:-5]+'_bkgsub.fits'
-    if 'rkimg' in imgs:
-        rkbkgsub=imgs['rkimg'][:-5]+'_bkgsub.fits'
-        
-    if idname == '':
-        idname = raw_input('Please enter an ID name for sources: ')
-    
-    if outprefix == '':
-        outname=clname+'_catalog.fits'
-    else:
-        outname=outprefix+'.fits'
-    
-    if kpno == True:
-        abconvb = vega2AB['bkpno']
-    else:
-        abconvb = vega2AB['bctio']
+    bkg_imgs, catalogs = {}, {}
+    for filter in imgs.keys():
+        root_name = imgs[filter].split('.')[0]
+        bkg_imgs[filter] = '{}_bkgsub.fits'.format(root_name)
+        catalogs[filter] = '{}_.cat'.format(root_name)
+
+        for test in [bkg_image[filter], catalogs[filter]]:
+            if not os.path.exists(test):
+                raise FileNotFoundError('{} not found! Are you sure you ran Source Extractor?'.format(test))
+
+        if filter is detection_image:
+            segmentation_map = '{}_segmap.fits'.format(root_name)
+            if not os.path.exists(segmentation_map):
+                raise FileNotFoundError('{} not found! Are you sure you ran Source Extractor?'.format(segmentation_map))
+
+    output_file = '{}_catalog.fits'.format(cluster_name)
+
+    ab_conv = const.Constants().vega_to_ab()
+    ab_conv['b'] = ab_conv['bkpno'] if kpno else ab_conv['bctio']
         
     #Merge the SExtractor photometry from different filters into one dictionary
     print '\n---------\nMERGING CATALOGS\n---------\n'
@@ -917,9 +866,15 @@ def main(b="", v="", r="", i="", z="", k="", rb="", rk="", imglist='', rsegmap="
     (foo1,foo2)=np.loadtxt(r, usecols = (0,1), unpack=True, comments= '#')
     nsrcs=len(foo1)
 
+    # ----------------------------------
+    # Get LDP information from MegaCat |
+    # ----------------------------------
+
     #Add in LDP redshifts
-    megatab=pf.open(megacat)
-    megadat=megatab[1].data
+    if use_megacat and ('MEGACAT' in os.environ):
+        with fits.open(os.environ['MEGACAT']) as mega_hdu:
+            mega_table = mega_hdu[1].data
+
     (mzldp,mqual)=(megadat['zldp'],megadat['q'])
 
     wfiSky=coordinates.SkyCoord(ra=data['ra']*u.degree, dec=data['dec']*u.degree)
