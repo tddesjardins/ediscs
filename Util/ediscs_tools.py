3262db771cff5f6e19cb1332127d90c7e603a2c0
Tyler Desjardins <desjard@stsci.edu>
2018-06-10 08:25:52 -0400

    Used sfdmap for E(B-V) values

--- Util/ediscs_tools.py
+++ Util/ediscs_tools.py
@@ -4,10 +4,12 @@ but have been moved here to simplify that code.
 """
 
 from astropy import units as u
-from astroquery.irsa_dust import IrsaDust
 from astropy import coordinates as coords
+from sfdmap import ebv
 
 import numpy as np
+import random
+import os
 
 
 def get_zp(flux, mag):
@@ -57,23 +59,24 @@ def fixed_mag_color(x, zp, type):
     return magnitude
 
 
-def labbe_sigfunc(N, s, a, b):
+def labbe_sigfunc(n, s, a, b):
     """
     PURPOSE
     -------
     Function from equation 3 from Labbe et al. (2003), ApJ, 125, 1107.
     """
 
-    value = N * s * (a + (b * N))
+    value = n * s * (a + (b * n))
 
     return value
 
 
-def get_ebv(ra, dec, radius=2*u.deg, frame='fk5'):
+def get_ebv(ra, dec, frame='fk5'):
     """
     PURPOSE
     -------
-    Determine the E(B-V) value from the Schlegel dust maps averaged over some radius.
+    Determine the E(B-V) value from the Schlegel dust maps. Must set SFD_DIR environment
+    variable pointing to the Schlegel map FITS files directory prior to running.
 
     INPUTS
     ------
@@ -86,25 +89,22 @@ def get_ebv(ra, dec, radius=2*u.deg, frame='fk5'):
         The declination of the source. This can be specified as either a string or an
         astropy.units object. If it is a string, it must specify the units, e.g., '-12d11m53s'.
 
-    radius (astropy.units object):
-        The radius over which the E(B-V) is averaged. The input value is an astropy.units
-        object (e.g., 2*u.degree). The acceptable range is 2 - 37.5 degrees, and the default
-        is 2 degrees.
-
     frame (str):
         Name of the coordinates frame. Default is 'fk5'.
 
     RETURNS
     -------
     ebv (float):
-        E(B-V) reddening at the source coordinates averaged over the radius.
+        E(B-V) reddening at the source coordinates.
     """
 
-    galcoord = coords.SkyCoord(ra=ra, dec=dec, frame=frame)
-    extinct_tab = IrsaDust.get_query_table(galcoord, section='ebv', radius=radius)
-    ebv = extinct_tab['ext SFD mean'][0]
+    if 'SFD_DIR' not in os.environ:
+        raise OSError('Must set SFD_DIR environment variable!')
 
-    return ebv
+    galcoord = coords.SkyCoord(ra=ra, dec=dec, frame=frame).galactic
+    ebv_value = ebv(galcoord) / 0.86
+
+    return ebv_value
 
 
 def matchxy(x1, y1, x2, y2, tol=0.1):
@@ -220,3 +220,44 @@ class MagTools(object, flux=0.0, mag=0.0, zp=0.0, ab_conv=0.0):
         calib_flux = self.mag_to_flux(ujy=True)
 
         return calib_flux
+
+
+def random_sample(catalog, filters, output1='randomFirst.fits', output2='randomSecond.fits', class_star=0.3, q=4):
+    """
+    PURPOSE: Generate a random sample of galaxies from the photometric catalog of a single cluster.
+    \tAlso generate a second FITS file of the other galaxies not in the random sample.'
+
+    INPUTS:
+    \tcatalog   - FITS photometric catalog
+    \tfilters   - string list of the filter names (e.g., BVRIzK)
+    \toutput1   - Name of random sample FITS catalog
+    \toutput2   - Name of FITS catalog with galaxies NOT in the random sample
+    \tclassStar - SExtractor class_star value to perform cut on (selects objects with class_star < this value)
+    \tQ         - LDP quality flag for selecting sources
+
+    RETURNS: None.
+    """
+
+    select = ('class_StarR < {} & Q5_7 == {} & '.format(class_star, q) +
+              ' & '.join(['sexflag{} == 0'.format(x) for x in filters]))
+
+    data = Table.read(catalog).to_pandas()
+    trim_data = data.query(select)
+
+    idx = xrange(len(trim_data))
+    sample = random.sample(idx, int(np.ceil(len(trim_data) / 2.)))
+    inverse = []
+    for x in idx:
+        if x not in sample:
+            inverse.append(x)
+    first = trim_data.iloc[sample]
+    second = trim_data.iloc[inverse]
+
+    fits_cols = list(trim_data.columns.values)
+    first_dict = {col_names[x]: first[fits_cols[x]].values for x in range(len(fits_cols))}
+    second_dict = {col_names[x]: second[fits_cols[x]].values for x in range(len(fits_cols))}
+    gals_dict = {col_names[x]: trim_data[fits_cols[x]].values for x in range(len(fits_cols))}
+
+    Table(first_dict, names=col_names, dtype=Fdtype).write(output1, format='fits', overwrite=True)
+    Table(second_dict, names=col_names, dtype=Fdtype).write(output2, format='fits', overwrite=True)
+    Table(gals_dict, names=col_names, dtype=Fdtype).write('galaxies.fits', format='fits', overwrite=True)
